e09b9124d885272dd58c2d15977c15146f26d78a
pedrowilian <pedrowbevilacqua@gmail.com>
2026-02-21 10:19:15 -0300

    Aula 1 - Intro JS

---
+++ intro_js/index.js
@@ -0,0 +1,31 @@
+
+
+// Declarando com o var
+// var nasceu a muito tempo(dar prefêrencia por let do que var)
+var c = 2
+c = 3
+console.log(c)
+
+//declarando variáveis 
+let a = 2
+console.log(a)
+let passouDeAno = true
+console.log(passouDeAno)
+a = 3
+console.log(a)
+let b
+console.log(b)
+b = 1.5
+console.log(b)
+
+// Para saber o tipo da variável
+const idade = 17
+console.log(typeof(idade))
+
+// Se é const só pode atribuir o valor ao criar a variável
+const nome = 'José'
+console.log(nome)
+
+// nome = 'José Silva' -> isso irá dar erro
+// Primeiro Hello World
+console.log('Hello World!')
