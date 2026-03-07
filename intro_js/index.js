1b86d5aeaaa07a71342743874c9ab8d74e9f5c23
pedrowilian <pedrowbevilacqua@gmail.com>
2026-03-07 09:22:20 -0300

    learn(ap01-js): encerra codigo 1.5.2
    - agora vimos funcoes que sao chamadas sobre vetores e que recebem funcoes como parametro

--- intro_js/index.js
+++ intro_js/index.js
@@ -1,13 +1,42 @@
+
+
+//listas/vetores/arrays/arranjos
+// const numeros = [1, 2, 3, 4]
+// const res = numeros.reduce((ac, v) => ac + v)
+// // console.log(res)
+// const nomes = ['Ana Maria', 'Antonio', 'Rodrigo', 'Cristina', 'Alex']
+// const res = nomes.every(n => n.startsWith("A"))
+// const res2 = nomes.some(n => n.startsWith("A"))
+// console.log(res)
+//[A, A, R, C, A]
+// const iniciais = nomes.map(function(nome){return nome.charAt(0)})
+// console.log(iniciais)
+// const apenasComA = nomes.filter(nome => nome.startsWith('A'))
+// console.log(apenasComA)
+
+
+// const f1 = () => {}
+// const f2 = function(){}
+// v1 = []
+// console.log(v1.length)
+// v1[0] = 'abc'
+// console.log(v1.length)
+// v1[10] = 2.5
+// console.log(v1.length)
+// console.log(v1)
+// for(let i = 0; i < v1.length; i++)
+//     console.log(v1[i])
+
 // funções 
 // function e arrow function
 // não existe sobrecarga de função, ou seja, não é possível criar duas funções com o mesmo nome, mesmo que tenham parâmetros diferentes
 // arrow function
-const eAgora = () => {1}
-const somar = (a, b) => a + b
-somar(2, 3) 
+// const eAgora = () => {1}
+// const somar = (a, b) => a + b
+// somar(2, 3) 
 
-const falarOi = nome => console.log('Oi, ' + nome + '!')
-falarOi('Maria')
+// const falarOi = nome => console.log('Oi, ' + nome + '!')
+// falarOi('Maria')
 
 // const triplo = function(x = 5){
 //     return 3 * x
