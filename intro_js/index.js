39fd5a441d89e37a9b38512d6c542a2524f4879c
pedrowilian <pedrowbevilacqua@gmail.com>
2026-03-07 09:30:37 -0300

    learn(ap01-js): encerra codigo 2.1.2

--- intro_js/index.js
+++ intro_js/index.js
@@ -1,3 +1,33 @@
+// JSON: JavaScript Object Notation
+// // Uma pessoa que se chama João e tem 17 anos
+// let pessoa = {
+//     nome: 'João',
+//     idade: 17
+// }
+
+// console.log(pessoa.nome)
+// console.log(pessoa['idade'])
+
+//Uma pessoa que se chama Maria, tem 21 anos, e mora na Rua B, número 50
+let pessoa = {
+    nome: 'Maria',
+    idade: 17,
+    endereco:{
+        rua: 'Rua B',
+        numero: 50
+    }
+}
+
+console.log(pessoa.nome)
+console.log(pessoa.idade)
+console.log(pessoa.endereco.rua)
+console.log(pessoa.endereco.numero)
+
+pessoa.nome = 'Ana Maria'
+console.log(pessoa.nome)
+
+
+// Closure
 // const eAgora = () => {
 //     let cont = 1
 //     const f1 = () => console.log(cont)
